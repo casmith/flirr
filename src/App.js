dd58d2abe84fe407818ca819fb5ee790bd3071b9
Clay Smith <uhclay@gmail.com>
2020-11-10 12:16:04 -0800

    Implement routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import Queue from './Queue';
 import AddToQueue from './AddToQueue';
+import Menu from './Menu';
+import Queue from './Queue';
 
 function App() {
     return (
         <div>
-            <AddToQueue />
-            <Queue />
+            <Router>
+                <Menu />
+                <Switch>
+                    <Route exact path="/">
+                        <Redirect to="/search" />
+                    </Route>
+                    <Route path="/search">
+                        <AddToQueue />          
+                    </Route>
+                    <Route path="/downloads">
+                        <Queue />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
