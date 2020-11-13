487de8af986f4ec46b2949937e457d5d5eec1c01
Clay Smith <uhclay@gmail.com>
2020-11-13 00:43:14 -0800

    Clean up search controller

--- src/Search.js
+++ src/Search.js
@@ -1,39 +1,20 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const exampleResults = [
-    {nick: 'someguy', filename: '01 - name of the song.mp3', artist: 'Starving Musician', album: 'Self Titled', song: 'Title Track'}
-];
-
 function Search({handleEnqueue}) {
     const [results, setResults] = useState([]);
     const [keywords, setKeywords] = useState("");
     
 
     const search = (keywords) => {
-        // const localResults = exampleResults
-        //     .filter(candidate => candidate.filename.includes(keywords))
-        // return Promise.resolve(localResults);
-
-
-
-
         return axios.get('/search?keywords=' + keywords)
-            .then(function (response) {
-            // handle success
-            console.log(response);
-            return response.data;
-            })
-            .catch(function (error) {
-            // handle error
-            console.log(error);
-            });
+            .then((response) => response.data)
+            .catch((error) => console.error(error))
     }
     
     const submitSearch = (e) => {
         e.preventDefault();
-        search(keywords)
-            .then((results) => setResults(results));
+        return search(keywords).then((results) => setResults(results));
     }
 
     const enqueue = (result) => handleEnqueue(result)
