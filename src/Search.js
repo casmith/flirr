ca0e184f20a5c3b259fd89cf1cf302d1c95a3bea
Clay Smith <uhclay@gmail.com>
2020-11-12 22:41:40 -0800

    Search via flirr-api

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const exampleResults = [
     {nick: 'someguy', filename: '01 - name of the song.mp3', artist: 'Starving Musician', album: 'Self Titled', song: 'Title Track'}
@@ -10,15 +11,29 @@ function Search({handleEnqueue}) {
     
 
     const search = (keywords) => {
-        const localResults = exampleResults
-            .filter(candidate => candidate.filename.includes(keywords))
-        return Promise.resolve(localResults);
+        // const localResults = exampleResults
+        //     .filter(candidate => candidate.filename.includes(keywords))
+        // return Promise.resolve(localResults);
+
+
+
+
+        return axios.get('/search?keywords=' + keywords)
+            .then(function (response) {
+            // handle success
+            console.log(response);
+            return response.data;
+            })
+            .catch(function (error) {
+            // handle error
+            console.log(error);
+            });
     }
     
     const submitSearch = (e) => {
         e.preventDefault();
         search(keywords)
-        .then((results) => setResults(results));
+            .then((results) => setResults(results));
     }
 
     const enqueue = (result) => handleEnqueue(result)
@@ -31,7 +46,7 @@ function Search({handleEnqueue}) {
             </label>
             <h2>Results</h2>
             <div>
-                {results.map((result, i) => (<div key={i}>result: {result.filename} <button onClick={() => enqueue(result)}>Enqueue</button></div>))}
+                {results.map((result, i) => (<div key={i}>{result.filename} <button onClick={() => enqueue(result)}>Enqueue</button></div>))}
             </div>
         </form>
     )
