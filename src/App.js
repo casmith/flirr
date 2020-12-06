22ed15284f3e71698a1b633341a14c0a59449590
Clay Smith <uhclay@gmail.com>
2020-12-05 17:00:33 -0800

    Add bot status to the UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
@@ -6,18 +6,30 @@ import AddToQueue from './AddToQueue';
 import Menu from './Menu';
 import Queue from './Queue';
 import Search from './Search';
-
+import axios from 'axios';
 
 function App() {
 
     const [queue, setQueue] = useState([])
+    const [status, setStatus] = useState({})
 
     const handleEnqueue = (item) => queue.includes(item) || setQueue([].concat(queue, item));
+    useEffect(() => {
+        axios.get('/api/status').then(response => response.data)
+            .then(status => setStatus(status))
+    }, [true])
+    
+
+    const statusElement = status.connected ? 
+        <div class="status">{status.nick} connected to {status.serverName} in channel {status.channel}</div> : 
+        <div class="status">Not connected</div>
 
     return (
         <div>
+            
             <Router>
                 <Menu downloads={queue.length} />
+                {statusElement}
                 <Switch>
                     <Route exact path="/">
                         <Redirect to="/search" />
