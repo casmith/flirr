9425bd8caacadb9654108de466c4d5ef17e4f38f
Clay Smith <uhclay@gmail.com>
2020-12-13 23:35:59 -0800

    Convert nav bar and search input to material-ui

--- src/Menu.js
+++ src/Menu.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Badge from '@material-ui/core/Badge';
 import './Menu.css';
 
 function Menu({downloads}) {
+    const location = useLocation();
     return (
-        <nav>
-            <ul>
-                <li><Link to="/search">Search</Link></li>
-                <li><Link to="/downloads">Downloads{downloads ? ` (${downloads})` : ''}</Link></li>
-            </ul>
-        </nav>
-        )
+        <AppBar position="static">
+            <Tabs value={location.pathname}>
+                <Tab value="/search" label="Search" component={Link} to="/search" />
+                <Tab value="/downloads" label={<Badge badgeContent={downloads} color="secondary">Downloads</Badge>} component={Link} to="/downloads"> ()</Tab>
+            </Tabs>
+        </AppBar>
+    )
 }
 
 export default Menu;
--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import PersonIcon from '@material-ui/icons/Person';
 
 function Search({handleEnqueue}) {
     const [results, setResults] = useState([]);
@@ -37,16 +40,13 @@ function Search({handleEnqueue}) {
 
     return (
         <form onSubmit={submitSearch}>
-            <label>Search:
-                <input name="search" onChange={e => setKeywords(e.target.value)}></input>
-                <input type="submit" value="Submit" />
-            </label>
+            <TextField id="standard-search" label="Search field" type="search" onChange={e => setKeywords(e.target.value)} />
             <h2>Results</h2>
             {inProgress && (<div>Searching...</div>)}
             <div>
             {!inProgress && (!!results.length || (<div>No search results</div>))}
             {results.map((result, i) => (
-                <div key={i}>{result.tracks[0].nick}{users.includes(result.tracks[0].nick) || ' (Offline)'} {result.album} {result.filename} <button onClick={() => enqueue(result)}>Enqueue</button>
+                <div key={i}>{users.includes(result.tracks[0].nick) ? <PersonIcon color="primary" /> : <PersonIcon color="disabled" />}{result.tracks[0].nick} {result.album} {result.filename} <Button variant="contained" color="primary" onClick={() => enqueue(result)}>Enqueue</Button>
                 {result.tracks.map(track => (
                     <div>{track.filename}</div>
                 ))}
