2f996351c6b29bbf27c0d199894209bc28f0750a
Clay Smith <casmith@users.noreply.github.com>
2020-12-20 03:43:21 -0800

    Optimize imports

--- src/Queue.js
+++ src/Queue.js
@@ -1,6 +1,5 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import './Queue.css'
+import React from 'react';
+import './Queue.css';
 
 function Queue({queue}) {
 
